66fc176bea56c7cef4dec2e9c1f2b9a1b6bbbdb1
Panos Mavrogiorgos <pmav99@gmail.com>
2019-10-24 17:17:18 +0200

    Add run_concurrent.py

---
+++ run_concurrent.py
@@ -0,0 +1,30 @@
+import sys
+
+
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import pandas as pd
+
+import jeodpp.inter
+import timer
+from run_serial import *
+
+
+
+def main():
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python run_single_py NO_PROCESSES NO_FILES")
+
+    no_processes = int(sys.argv[1])
+    no_files = int(sys.argv[2])
+
+    df = get_query_params()
+
+    with ProcessPoolExecutor(no_processes) as executor:
+        future_processes = [executor.submit(create_tile_from_row, df.loc[index]) for index in df.sample(no_files).index]
+
+    results = [f.result() for f in as_completed(future_processes)]
+    for result in results:
+        assert len(result) > 800
+
+if __name__ == "__main__":
+    main()
